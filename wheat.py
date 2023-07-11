e96d7c2c4f11e6ccf75e518f8ebd6b207c26b710
Evan <evan.er545@gmail.com>
2023-07-11 12:09:36 -0700

    added dummy key

---
+++ wheat.py
@@ -0,0 +1,47 @@
+from hashlib import sha256
+
+MESSAGE = "Hello"
+OUTPUT_FILE = "wheat.txt"
+AUTH_KEY_FILE = "KEY.txt"
+
+
+def read_key(filename):
+    with open(filename, "r") as f:
+        key = f.read()
+    return key
+
+
+def main():
+    # Read the key from the file
+    KEY = read_key(AUTH_KEY_FILE)
+
+    # Convert the message to a list of bits
+    message_chars = [ord(c) for c in MESSAGE]
+    message_bits = []
+    for char in message_chars:
+        message_bits += [int(b) for b in bin(char)[2:].zfill(8)]
+
+
+    with open(OUTPUT_FILE, "w") as f:
+        serial = 0
+
+        for bit in message_bits:
+
+            # calculate mac for each bit: mac = SHA256(serial, bit, key)
+            mac_components = str(serial) + str(bit) + str(KEY)
+            mac = sha256(mac_components.encode('utf-8')).hexdigest()
+
+            # encode serial as 32 bit hex string
+            encoded_serial = hex(serial)[2:].zfill(8)
+
+            # add new line if serial is not 0
+            if serial != 0:
+                f.write("\n")
+
+            # write serial,bit,mac to file
+            f.write(",".join([str(encoded_serial), str(bit), mac]))
+
+            serial += 1
+
+if __name__ == "__main__":
+    main()
